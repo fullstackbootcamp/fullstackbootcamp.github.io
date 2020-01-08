d3294c41a385565a0c543088776565a0c85ff394
Marc Dagatan <marc.dagatan@gmail.com>
2020-01-08 16:38:04 +0800

    Update powered by section

--- src/components/homeSections/PoweredBy.js
+++ src/components/homeSections/PoweredBy.js
@@ -1,13 +1,35 @@
 import React from 'react';
 import styled from '@emotion/styled';
-import { SectionHeader } from '../typo';
+import { graphql, useStaticQuery } from 'gatsby';
+import Image from 'gatsby-image';
 
+import { SectionHeader } from '../typo';
 import { rhythm, scale } from '../../utils/typography';
 import { Container } from '../components';
 
-export default () => (
-  <Container>
-    <h2>Powered by</h2>
-    <h4>together with</h4>
-  </Container>
-);
+const ImageContainer = styled.div`
+  margin-bottom: ${rhythm(1)};
+`;
+
+export default () => {
+  const query = useStaticQuery(graphql`
+    query {
+      fs: file(absolutePath: { regex: "/fullsuite_logo.png/" }) {
+        childImageSharp {
+          fixed(height: 100) {
+            ...GatsbyImageSharpFixed
+          }
+        }
+      }
+    }
+  `);
+
+  return (
+    <Container>
+      <h2>Powered by</h2>
+      <ImageContainer>
+        <Image fixed={query.fs.childImageSharp.fixed} />
+      </ImageContainer>
+    </Container>
+  );
+};
