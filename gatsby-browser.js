a1cf35a63ad552f866db6a718604ee17da719778
Marc Dagatan <marc.dagatan@gmail.com>
2020-01-09 12:06:41 +0800

    Updates

--- gatsby-browser.js
+++
@@ -1,7 +0,0 @@
-// custom typefaces
-import 'typeface-ubuntu';
-import 'typeface-ubuntu-mono';
-
-import { wrapRootElement as wrap } from './wrap-root-element';
-
-export const wrapRootElement = wrap;
---
+++ styles-16f6adb2359ef4e74f0d.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[6],{"5/sT":function(n,o,w){},gcnl:function(n,o,w){}}]);
+//# sourceMappingURL=styles-16f6adb2359ef4e74f0d.js.map
